40bfbc62d00ff47fef6286c861e401ac30576eab
Julie B. <no-reply@bbjubjub.fr>
2024-09-06 10:03:21 +0200

    rotki: limit platforms

--- pkgs/rotki/default.nix
+++ pkgs/rotki/default.nix
@@ -9,4 +9,6 @@ appimageTools.wrapType2 rec {
     url = "https://github.com/rotki/rotki/releases/download/${version}/rotki-linux_x86_64-${version}.AppImage";
     hash = "sha256-6XaF5H+WF6jdiBm3NHbI5IEzYK/wZMWrVPky0kS4msU=";
   };
+
+  meta.platforms = [ "x86_64-linux" ];
 }
